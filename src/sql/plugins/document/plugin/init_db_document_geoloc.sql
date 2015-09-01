f23874ab9e4f5891440c49e08cd1289aac623f05
Jon Harper <jon.harper87@gmail.com>
2015-09-01 16:45:42 +0200

    DOCGEOLOC-1 - Add geoloc fields to documents

---
+++ src/sql/plugins/document/plugin/init_db_document_geoloc.sql
@@ -0,0 +1,4 @@
+INSERT INTO document_attr_type (code_attr_type,name_key,description_key,manager_class) VALUES ('geoloc','module.document.geoloc.attributeType.geoloc.name','module.document.geoloc.attributeType.geoloc.description','fr.paris.lutece.plugins.document.modules.geoloc.service.attributes.GeolocManager');
+
+INSERT INTO document_attr_type_parameter (code_attr_type,parameter_name,parameter_label_key,parameter_index,parameter_description_key,parameter_default_value) VALUES ('geoloc','style','module.document.geoloc.attributeType.geoloc.parameter.style.label',1,'module.document.geoloc.attributeType.geoloc.parameter.style.description','1740');
+INSERT INTO document_attr_type_parameter (code_attr_type,parameter_name,parameter_label_key,parameter_index,parameter_description_key,parameter_default_value) VALUES ('geoloc','icon','module.document.geoloc.attributeType.geoloc.parameter.icon.label',2,'module.document.geoloc.attributeType.geoloc.parameter.icon.description','default');
